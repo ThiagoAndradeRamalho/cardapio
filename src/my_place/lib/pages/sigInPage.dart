ea2642241c0bc0a483bb77196b1a8d86b78f4b63
Thiago Andrade <thiandrade79@gmail.com>
2025-01-29 14:14:29 -0300

    Subindo configurações iniciais

---
+++ src/my_place/lib/pages/sigInPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SigInPage extends StatefulWidget {
+  SigInPage({Key ?key}) : super(key: key);
+
+  @override
+  State<SigInPage> createState() => _SigInPageState();
+}
+
+class _SigInPageState extends State<SigInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
